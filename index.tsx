ea72163bd4a1d15d103629cb8435ac44a8c62222
Pradhumn1999 <pradhumn.choubey@nibavlifts.com>
2024-10-08 14:08:47 +0530

    interview prepration

---
+++ index.tsx
@@ -0,0 +1,18 @@
+useref
+usecallback
+usememo
+useeffect
+usestate:-every on single state change it will render full component
+
+ 
+<Fragment>, alternatively written as <>...</>, lets you group multiple JSX nodes together.
+<Profiler> lets you measure rendering performance of a React tree programmatically.
+<Suspense> lets you display a fallback while the child components are loading.
+<StrictMode> enables extra development-only checks that help you find bugs early.
+
+
+
+
+
+# error boundry compoent:- we rap the small chunk of code so it that spacific data did not come it will show message.
+
